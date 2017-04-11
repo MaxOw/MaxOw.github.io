d01e491b80e86bdf32981ee5754b91170b3e6caa
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-04-11 16:27:31 +0000

    Add nix source filtering

--- blog.nix
+++ blog.nix
@@ -2,7 +2,13 @@
 mkDerivation {
   pname = "blog";
   version = "0.1.0";
-  src = ./.;
+  src = builtins.filterSource (path: type:
+          baseNameOf path != "result" &&
+          baseNameOf path != "posts"  &&
+          baseNameOf path != "images" &&
+          baseNameOf path != "_site"  &&
+          baseNameOf path != "_cache" &&
+          baseNameOf path != ".git") ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [ base hakyll pandoc ];
