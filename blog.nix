994313739b8a0ea5f1a2e0292e1762c9d6465cb1
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-04-11 17:15:38 +0000

    Caching not worth it, nuke it

--- blog.nix
+++ blog.nix
@@ -3,11 +3,12 @@ mkDerivation {
   pname = "blog";
   version = "0.1.0";
   src = builtins.filterSource (path: type:
-          baseNameOf path != "result" &&
-          baseNameOf path != "posts"  &&
-          baseNameOf path != "images" &&
-          baseNameOf path != "_site"  &&
-          baseNameOf path != "_cache" &&
+          baseNameOf path != "result"  &&
+          baseNameOf path != "posts"   &&
+          baseNameOf path != "images"  &&
+          baseNameOf path != "_site"   &&
+          baseNameOf path != "_cache"  &&
+          baseNameOf path != ".travis" &&
           baseNameOf path != ".git") ./.;
   isLibrary = false;
   isExecutable = true;
