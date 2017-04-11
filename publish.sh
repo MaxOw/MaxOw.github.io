c010785aeef7b1125ba530148abaa52a657575f4
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-04-11 21:35:06 +0000

    Publish only on command

--- publish.sh
+++ publish.sh
@@ -1,12 +1,15 @@
-
-export REMOTE=$(git config remote.origin.url | sed 's/.*:\/\///')
-git clone https://${REMOTE}
-cp _site/* maxow.github.io/ -r
-cd maxow.github.io
-git config --global user.email "$GIT_EMAIL"
-git config --global user.name  "$GIT_NAME"
-git remote add github https://${GH_TOKEN}@${REMOTE}
-git add --all
-git status
-git commit -m "Built by Travis ( build nr $TRAVIS_BUILD_NUMBER )"
-git push github master:master &>/dev/null
+if ! echo "$TRAVIS_COMMIT_MESSAGE" \
+   | grep -qvE  '\[publish\]'
+then
+  export REMOTE=$(git config remote.origin.url | sed 's/.*:\/\///')
+  git clone https://${REMOTE}
+  cp _site/* maxow.github.io/ -r
+  cd maxow.github.io
+  git config --global user.email "$GIT_EMAIL"
+  git config --global user.name  "$GIT_NAME"
+  git remote add github https://${GH_TOKEN}@${REMOTE}
+  git add --all
+  git status
+  git commit -m "Built by Travis ( build nr $TRAVIS_BUILD_NUMBER )"
+  git push github master:master &>/dev/null
+fi
